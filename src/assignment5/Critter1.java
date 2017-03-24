7b451ab566e03b7721c22e17c9fe756b0c637093
xirothehero <ali.kedwaii@utexas.edu>
2017-03-23 23:11:18 -0500

    updated Crits

---
+++ src/assignment5/Critter1.java
@@ -0,0 +1,98 @@
+package assignment4;
+
+/* CRITTERS Critter1.java
+ * EE422C Project 4 submission by
+ * Hamza Khatri
+ * hak533
+ * 16220
+ * Slip days used: <0>
+ * Mohammad Kedwaii
+ * mak3799
+ * 16238
+ * Slip days used: <0>
+ * Spring 2017
+ */
+
+import java.util.List;
+import javafx.scene.paint.Color;
+
+public class Critter1 extends Critter {
+
+
+    public Critter1(){
+        super();
+    }
+
+    /**
+     * @return ASCII representation of critter
+     */
+    public String toString() { return "1"; }
+
+
+    /** Makes baby in random direction */
+    public void doTimeStep() {
+
+        Critter1 baby = null;
+        try {
+            baby = this.getClass().newInstance();
+        }
+        catch (InstantiationException | IllegalAccessException e){}
+
+        reproduce(baby, Critter.getRandomInt(7));
+    }
+
+
+    /**
+     *  Makes baby in random direction
+     * Then fights (to the death)
+     * @param opponent String that tells you type of opponent
+     */
+    public boolean fight(String opponent){
+
+        Critter1 baby = null;
+        try {
+            baby = this.getClass().newInstance();
+        }
+        catch (InstantiationException | IllegalAccessException e){}
+
+        int num = Critter.getRandomInt(7);
+
+        look(num, false);
+        reproduce(baby, num);
+
+        return true;
+    }
+
+    /**
+     * Displays total number of critters, along with average energy
+     * @param critters List of Critter1 objects in population
+     */
+    public static void runStats (List<Critter> critters) {
+        System.out.print("Total Critter1's: " + critters.size() + "\t");
+        if (critters.size() > 0) {
+            int avgNRG = 0;
+            for (Critter c : critters) {
+                avgNRG += ((Critter1)c).getEnergy();
+            }
+            avgNRG /= critters.size();
+            System.out.print("Avg Energy: " + avgNRG);
+        }
+        System.out.println();
+    }
+
+    @Override
+    public CritterShape viewShape() {
+        return CritterShape.TRIANGLE;
+    }
+
+
+    @Override
+    public Color viewOutlineColor() {
+        return Color.MAROON;
+    }
+
+    @Override
+    public Color viewFillColor() {
+        return Color.DARKCYAN;
+    }
+}
